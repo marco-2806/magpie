effc9b2973da890be94a9dbfa83d335d08124a27
Kuchen <daniel.schneeweis@a1.net>
2024-12-22 21:14:13 +0100

    + add a few things

--- frontend/src/app/scraper/scraper.component.ts
+++ frontend/src/app/scraper/scraper.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {LoadingComponent} from '../loading/loading.component';
 import {TooltipComponent} from '../tooltip/tooltip.component';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {NgForOf} from '@angular/common';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-scraper',
@@ -9,11 +12,41 @@ import {TooltipComponent} from '../tooltip/tooltip.component';
   imports: [
     MatIcon,
     LoadingComponent,
-    TooltipComponent
+    TooltipComponent,
+    MatTab,
+    NgForOf,
+    MatTabGroup,
+    ReactiveFormsModule
   ],
   templateUrl: './scraper.component.html',
   styleUrl: './scraper.component.scss'
 })
 export class ScraperComponent {
+  scraperForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.scraperForm = this.fb.group({
+      sources: this.fb.array([]),
+      scrapeInterval: new FormControl(10),
+      maxProxies: new FormControl(100),
+      scrapeThreads: new FormControl(5),
+      log: new FormControl('')
+    });
+  }
+
+  get sources(): FormArray {
+    return this.scraperForm.get('sources') as FormArray;
+  }
+
+  addSource() {
+    this.sources.push(this.fb.group({
+      url: [''],
+      delimiter: ['']
+    }));
+  }
+
+  onSubmit() {
+    console.log(this.scraperForm.value);
+    // Save settings or trigger scraper logic here
+  }
 }
